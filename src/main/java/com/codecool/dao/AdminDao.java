31ea4aae915419fef718ac608515580a88f737aa
Alek <sforaaleksander@gmail.com>
2020-05-04 19:36:26 +0200

    refactor imports

--- src/main/java/com/codecool/dao/AdminDao.java
+++ src/main/java/com/codecool/dao/AdminDao.java
@@ -2,7 +2,8 @@ package com.codecool.dao;
 
 import com.codecool.models.Admin;
 
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
