9fca55dd80fa5853fe669b25e803cf8890c3e4a4
Alek <sforaaleksander@gmail.com>
2020-05-04 22:21:07 +0200

    implement login logic

--- src/main/java/com/codecool/session/Login.java
+++ src/main/java/com/codecool/session/Login.java
@@ -2,34 +2,55 @@ package com.codecool.session;
 
 import com.codecool.dao.AdminDao;
 import com.codecool.dao.CustomerDao;
+import com.codecool.dao.Dao;
 import com.codecool.models.Admin;
 import com.codecool.models.Customer;
 import com.codecool.ui.IO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
-public class Login {
-    private AdminDao adminsList;
-    private CustomerDao customersList;
-    private IO io;
+public class Login extends Dao {
+    private String userEmail;
+    private String userPassword;
+    private boolean isUserAdmin;
+    private String loggedUser;
 
-    public Login(IO io) {
-        adminsList = new AdminDao();
-        customersList = new CustomerDao();
-        this.io = io;
+    public Login(String userEmail, String userPassword) {
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
     }
 
-    public String userLogging() {
-        String userEmail = io.gatherInput("Email: ");
-        String userPassword = io.gatherInput("Password: ");
-        if (adminsList.getAdmins().stream().anyMatch(o -> o.getEmail().equals(userEmail))) {
-            return "";
+    public boolean loginAttempt() throws SQLException {
+        connect();
+        boolean adminMatch = adminLoginAttempt(userEmail, userPassword);
+        boolean customerMatch = customerLoginAttempt(userEmail, userPassword);
+        return adminMatch || customerMatch;
+    }
+
+    public boolean adminLoginAttempt(String userEmail, String userPassword) throws SQLException {
+        ResultSet emailResult = statement
+                .executeQuery("SELECT * FROM Admins WHERE Email = " + userEmail + "AND Password = " + userPassword + ";");
+        if (!emailResult.first()) {
+            isUserAdmin = false;
+            return false;
+        } else {
+            return true;
         }
-        return "";
     }
 
-    public boolean isUserAdmin(String userEmail) {
-        return adminsList.getAdmins().stream().anyMatch(o -> o.getEmail().equals(userEmail));
+    public boolean customerLoginAttempt(String userEmail, String userPassword) throws SQLException {
+        ResultSet emailResult = statement
+                .executeQuery("SELECT * FROM Customers WHERE Email = " + userEmail + "AND Password = " + userPassword + ";");
+        return emailResult.first();
     }
+
+
 }
+
+
+//    public boolean isUserAdmin(String userEmail) {
+//        return adminsList.getAdmins().stream().anyMatch(o -> o.getEmail().equals(userEmail));
+//    }
 
--- src/main/java/com/codecool/session/Session.java
+++ src/main/java/com/codecool/session/Session.java
@@ -2,15 +2,24 @@ package com.codecool.session;
 
 import com.codecool.ui.IO;
 
+import java.sql.SQLException;
+
 public class Session {
     private String loggedAs;
     private boolean loggedAsAdmin;
     private long sessionTime;
     private IO io;
 
-    public Session(){
+    public Session() {
         IO io = new IO();
-        Login login = new Login(io);
-        loggedAs = login.userLogging();
+        String userEmail = io.gatherInput("Email: ");
+        String userPassword = io.gatherInput("Password: ");
+        Login login = new Login(userEmail, userPassword);
+        try {
+            login.loginAttempt();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
