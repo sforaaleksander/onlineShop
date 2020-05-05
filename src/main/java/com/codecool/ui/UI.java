9fa1b5c723fb98d7b2fc12ed031b063dc9aa4f74
MateuszGolda <mateusz.golda1993@gmail.com>
2020-05-05 12:26:52 +0200

    add methods to print collections, and arrays, and to gather input

--- src/main/java/com/codecool/ui/UI.java
+++ src/main/java/com/codecool/ui/UI.java
@@ -1,13 +1,45 @@
 package com.codecool.ui;
 
+import java.util.Collection;
+
 public class UI {
     private IO io;
 
-    public UI(){
+    public UI() {
         io = new IO();
     }
 
-    public IO getIo() {
-        return io;
+    public void displayMenu() {
+        print(new String[] { "(1) Start new game",
+                             "(2) How to play",
+                             "(3) About authors",
+                             "(0) Exit" });
+    }
+
+    public void print(Collection<String> collection) {
+        collection.stream()
+                  .forEach(System.out::println);
+    }
+
+    public void print(String[] toPrint) {
+        for (String string : toPrint) {
+            System.out.print(string);
+        }
+    }
+
+    public void gatherEmptyInput(String message) {
+        io.gatherEmptyInput(message);
+    }
+
+    public String gatherInput(String message) {
+        return io.gatherInput(message);
+    }
+
+    public int gatherIntInput(String message, int rangeMin, int rangeMax) {
+        return io.gatherIntInput(message, rangeMin, rangeMax);
+    }
+
+    public void clearScreen() {
+        System.out.print("\033[H\033[2J");
     }
 }
