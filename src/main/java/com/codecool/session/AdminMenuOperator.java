43bee7b9455151625883d7af85c1600ad751f035
Alek <sforaaleksander@gmail.com>
2020-05-06 18:24:51 +0200

    refactor AdminMenuOperator, now methods use printFromDB method to print queries

--- src/main/java/com/codecool/session/AdminMenuOperator.java
+++ src/main/java/com/codecool/session/AdminMenuOperator.java
@@ -1,11 +1,9 @@
 package com.codecool.session;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import com.codecool.dao.OrderDao;
-import com.codecool.models.Order;
+
 import com.codecool.models.User;
 import com.codecool.ui.UI;
 
@@ -63,16 +61,16 @@ public class AdminMenuOperator extends MenuOperator {
         printFromDB("SELECT * FROM Users WHERE " + column + " LIKE '%" + toSearch + "%';");
     }
 
-    private List<Order> getAllOrders() {
-        return new OrderDao().getOrders("SELECT * FROM Orders;");
+    private void getAllOrders() {
+        printFromDB("SELECT * FROM Orders;");
     }
 
-    private List<Order> getOrdersContaining() {
+    private void getOrdersContaining() {
         String column;
         String toSearch;
         column = ui.gatherInput("Provide column: ");
         toSearch = ui.gatherInput("What to look for?: ");
-        return new OrderDao().getOrders("SELECT Order_status, Created_at, Paid_at, Name, Price FROM Orders"
+        printFromDB("SELECT Order_status, Created_at, Paid_at, Name, Price FROM Orders"
                                         + "JOIN Order_products ON Order_products.Id_order = Orders.Id JOIN Products ON"
                                         + "Products.Id = Order_products.Id_product WHERE " + column + " LIKE '%" + toSearch + "%';");
     }
