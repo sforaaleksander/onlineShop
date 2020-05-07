a51e0c2744dd48d2c65c44911bfe88b9c492627a
Dominik <domek99@gmail.com>
2020-05-07 15:11:53 +0200

    change registration to use insert func from Deo

--- src/main/java/com/codecool/session/Registration.java
+++ src/main/java/com/codecool/session/Registration.java
@@ -6,39 +6,28 @@ import com.codecool.ui.UI;
 import java.sql.SQLException;
 
 public class Registration extends Dao {
-    private String name;
-    private String surname;
-    private String email;
-    private String password;
-    private String phone;
     private UI ui;
+    private String[] values;
 
     Registration() {
         connect();
         ui = new UI();
         enterUserData();
-        insertIntoDataBase();
+        insertUser();
     }
 
     private void enterUserData() {
-        name = ui.gatherInput("Enter your name: ");
-        surname = ui.gatherInput("Enter your surname: ");
-        email = ui.gatherInput("Enter your email: ").toLowerCase();
-        password = ui.gatherInput("Enter your password: ");
-        phone = ui.gatherInput("Enter your phone: ");
+        String name = "'" + ui.gatherInput("Enter your name: ") + "'";
+        String surname = "'" + ui.gatherInput("Enter your surname: ") + "'";
+        String email = "'" + ui.gatherInput("Enter your email: ").toLowerCase() + "'";
+        String password = "'" + ui.gatherInput("Enter your password: ") + "'";
+        String phone = "'" + ui.gatherInput("Enter your phone: ") + "'";
+        String Id_role = "2";
+        values = new String[]{name, surname, email, password, phone, Id_role};
     }
 
-    private void insertIntoDataBase() {
-        String sql = "INSERT INTO Users (name,surname, email, password, phone, Id_role)" +
-                "VALUES ('" + name + "','" + surname + "','" + email + "','" + password + "','" + phone + "','" + 2 + "');";
-        try {
-            statement.executeUpdate(sql);
-            ui.print("");
-            statement.close();
-            connection.close();
-            ui.print("Registration successful");
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
+    private void insertUser() {
+        String[] columns = {"name", "surname", "email", "password", "phone", "Id_role"};
+        insert("Users", columns, values);
     }
 }
