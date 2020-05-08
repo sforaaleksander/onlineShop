3f383743b581ac517e0b87fb6f4e2ccd73e02fe5
MateuszGolda <mateusz.golda1993@gmail.com>
2020-05-08 10:09:04 +0200

    validate email when registering new user

--- src/main/java/com/codecool/session/Registration.java
+++ src/main/java/com/codecool/session/Registration.java
@@ -24,6 +24,10 @@ public class Registration extends Dao {
             ui.gatherEmptyInput("User with this email already exists");
             return;
         }
+        if (!isValidEmailAddress(email)) {
+            ui.gatherEmptyInput("Invalid email address");
+            return;
+        }
         String password = ui.gatherInput("Enter your password: ");
         String name = ui.gatherInput("Enter your name: ");
         String surname = ui.gatherInput("Enter your surname: ");
@@ -31,5 +35,12 @@ public class Registration extends Dao {
         String Id_role = "2";
         String[] values = { name, surname, email, password, phone, Id_role };
         new UserDao().insertUser(values);
+    }
+
+    public boolean isValidEmailAddress(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
     }
 }
